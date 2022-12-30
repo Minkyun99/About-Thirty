6d5abc36d3765bab4e547d5af0e7fb3dcd39ad4e
Minkyun99 <kyung865821@gmail.com>
2022-12-30 17:51:36 +0900

    20221230

--- vue/chating/src/router/index.js
+++ vue/chating/src/router/index.js
@@ -7,13 +7,25 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+
+  {
+    path: '/chating',
+    name: 'chating',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/chating.vue')
+  },
+
   {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/chating.vue')
   }
 ]
 
---
+++ vue/chating/src/views/chating.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="container">
+    안녕하세요. 채팅창을 만들고 있는 중입니다...
+    <div>
+      <ul>
+        <li v-for="i in text_save" :key="i">{{ inValue }}</li>
+      </ul>
+    </div>
+    <div>
+      <input
+        type="text"
+        v-bind:value="inValue"
+        v-on:input="upValue"
+        v-on:keyup.enter="setData"
+      />
+      <button @click="setData">전송</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      inValue: '',
+      temp: '',
+      text_save: []
+    }
+  },
+
+  methods: {
+    upValue: function (e) {
+      this.inValue = e.target.value
+    },
+
+    setData: function () {
+      if (this.inValue) {
+        this.text_save.push(this.inValue)
+        this.temp = this.inValue
+        this.inValue = ''
+        this.text_save = ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+li {
+  list-style-type: none;
+}
+</style>
