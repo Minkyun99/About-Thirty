12bae78e29ab95527a9c127b2c76b08533026af8
leesibin <tlqls123@gmail.com>
2022-12-30 17:49:28 +0900

    12/30 한거없어요 주말에 열심히할게요봐줘...

---
+++ vue/vueman/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>로그인</h1>
+    <input type="text" value="닉네임" />
+    <button v-on@click="data">참여</button>
+  </div>
+</template>
+<script>
+import data from '../views/chat.vue'
+export default {
+  name: 'app',
+  data() {
+    return {
+      data: ''
+    }
+  },
+  methods: {}
+}
+</script>
---
+++ vue/vueman/src/views/chat.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>채팅창</h1>
+  </div>
+</template>
