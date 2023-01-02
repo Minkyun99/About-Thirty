07d15c52e535f1d4b03337b3b38002f611e9edc6
Minkyun99 <alsrud7412@naver.com>
2023-01-02 17:31:47 +0900

    20230102

--- vue/chating/src/main.js
+++ vue/chating/src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import io from 'socket.io-client'
+const socket = io('http://localhost:8080', { transports: ['websocket'] })
+
+const app = createApp(App)
+app.config.globalProperties.$socket = socket
+
+app.use(store).use(router).mount('#app')
--- vue/chating/src/views/chating.vue
+++ vue/chating/src/views/chating.vue
@@ -3,43 +3,62 @@
     안녕하세요. 채팅창을 만들고 있는 중입니다...
     <div>
       <ul>
-        <li v-for="i in setData" :key="i">{{ inValue }}</li>
+        <li v-for="data in arrmeg" :key="data">{{ data }}</li>
       </ul>
     </div>
     <div>
-      <input
-        type="text"
-        v-bind:value="inValue"
-        v-on:input="upValue"
-        v-on:keyup.enter="setData"
-      />
-      <button @click="setData">전송</button>
+      <input type="text" v-model="state" v-on:keyup.enter="add" />
+      <button @click="add">전송</button>
     </div>
   </div>
 </template>
 
 <script>
+import { getCurrentInstance, onMounted, reactive } from '@vue/runtime-core'
+
 export default {
-  name: 'app',
+  name: 'App',
   data() {
     return {
-      inValue: '',
-      temp: ''
-      // text_save: []
+      inmeg: '',
+      arrmeg: [],
+      meg: '',
+      megtext: ''
+    }
+  },
+
+  setup() {
+    const app = getCurrentInstance()
+    const $socket = app.appContext.config.globalProperties.$socket
+
+    const state = reactive({
+      message: '',
+      textarea: ''
+    })
+
+    function chatSubmit() {
+      $socket.emit('chat', {
+        message: state.message
+      })
+      state.message = ''
+    }
+
+    onMounted(() => {
+      $socket.on('chat', (data) => {
+        state.textarea += data.message + '\n'
+      })
+    })
+
+    return {
+      state,
+      chatSubmit
     }
   },
 
   methods: {
-    upValue: function (e) {
-      this.inValue = e.target.value
-    },
-
-    setData: function () {
-      if (this.inValue) {
-        // this.text_save.push(this.inValue)
-        this.temp = this.inValue
-        this.inValue = ''
-      }
+    add: function () {
+      this.arrmeg.push(this.state)
+      this.megtext = this.arrmeg
     }
   }
 }
