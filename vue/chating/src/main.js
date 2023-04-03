f58e772b0f095e0ca831c8a932b7c3acef76176f
Minkyun99 <alsrud7412@naver.com>
2023-04-03 22:30:00 +0900

    update

--- vue/chating/src/main.js
+++ vue/chating/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import { io } from 'socket.io-client'
 
 const app = createApp(App)
+const socket = io()
 app.config.globalProperties.$socket = socket
 
 app.use(router).use(store).mount('#app')
--- vue/chating/src/views/HomeView.vue
+++ vue/chating/src/views/HomeView.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2 @click="$router.push('/chating')">
+      Socket을 이용한 채팅 프로그램 개발
+    </h2>
+    <!-- <router-link
+      :to="{
+        name: 'chating',
+        params: { name: nickname }
+      }"
+    >
+    </router-link> -->
+    <input
+      v-model="nickname"
+      type="text"
+      placeholder="대화하실 닉네임을 입력해주세요."
+    />
+    <button @click="next_page">입력</button>
   </div>
 </template>
 
